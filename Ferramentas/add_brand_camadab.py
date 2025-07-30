02a061155b2e5061db0321eb9758544edad20692
Enzolinn <zanatta.enzolin@gmail.com>
2025-07-30 16:10:53 -0300

    adicionado ean_brand em todas as entradas camadab_base.json

--- Ferramentas/add_brand_camadab.py
+++ Ferramentas/add_brand_camadab.py
@@ -2,8 +2,25 @@ import json
 import re
 from collections import OrderedDict
 
+
+brand_ean_map = {
+    "PANTENE": "7500000000001",
+    "EUDORA": "7200000000002",
+    "ABOVE": "7300000000003",
+    "VULT": "7400000000004",
+    "MONANGE": "7500000000005",
+    "TRESEMME": "7600000000006",
+    "CLEAR": "7700000000007",
+    "SHOULDERS": "7110000000008",
+    "ELSEVE": "7800000000002",
+    "DOVE": "7891150000000",
+    "SEDA": "7900000000009",
+    "PALMOLIVE": "7100000000001",
+    "DARLING": "7130000000013"
+}
+
 # Lê o arquivo original
-with open('produtos.json', 'r', encoding='utf-8') as f:
+with open('camadab_base.json', 'r', encoding='utf-8') as f:
     dados = json.load(f)
 
 novo_dados = []
@@ -11,11 +28,14 @@ novo_dados = []
 # Reorganiza os campos para garantir que "ean" fique por último
 for item in dados:
     brand = item["mandatory_terms"][0] if item["mandatory_terms"] else ""
+    ean_brand = brand_ean_map.get(brand, "0")
+
     
     novo_item = OrderedDict()
     novo_item["mandatory_terms"] = item["mandatory_terms"]
-    ##novo_item["optional_terms"] = item["optional_terms"]
+    novo_item["optional_terms"] = item["optional_terms"]
     novo_item["brand"] = brand
+    novo_item["ean_brand"] = ean_brand
     novo_item["ean"] = item["ean"]
     
     novo_dados.append(novo_item)
@@ -31,5 +51,5 @@ json_formatado = re.sub(
 )
 
 # Salva no arquivo final
-with open('produtos_com_brand_tree.json', 'w', encoding='utf-8') as f:
+with open('produtos_com_ean_brand.json', 'w', encoding='utf-8') as f:
     f.write(json_formatado)
