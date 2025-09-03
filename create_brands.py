a55e97b6170d8dc4a9da1c44ad54fe67f6e2f418
Murilo Ossanes <murilossanes@hotmail.com>
2025-09-03 13:12:49 -0300

    Add downy

--- create_brands.py
+++ create_brands.py
@@ -15,7 +15,7 @@ def gerar_ean(base=7190000000000, indice=1):
     return str(base - indice)  # Decrementa os eans para evitar duplicatas(NÃO MEXER)
 
 # Lista de novas marcas
-novas_marcas = ["PANTENE","MONANGE"]
+novas_marcas = ["DOWNY"]
 
 # Descobrir quantos já existem para não repetir
 ultimo_indice = len(marcas_data)
